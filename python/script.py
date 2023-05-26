6785b2137bd3181aad04ce91d2ee1138dfac94d1
Adonis Rebano <earebano01@monccnb.ca>
2023-05-25 23:13:06 -0300

    ajouté des commentaires dans le script Python

--- python/script.py
+++ python/script.py
@@ -1,13 +1,18 @@
+#on importe les bibliothèques/modules nécessaires.
 import serial
 import psycopg2
 from datetime import date, datetime
 
+#on met en place la connexion série avec le port et le débit (baud rate) spécifiés
 ser = serial.Serial('COM3', 9600)
 
+#on établit une connexion à la base de données PostgreSQL en utilisant les paramètres ci-dessous
 conn = psycopg2.connect(host='localhost', port='5432', database='test1', user='postgres', password='admin')
 
+#on crée un objet curseur pour exécuter les requêtes SQL sur la base de données.
 cursor = conn.cursor()
 
+#on exécute une requête SQL pour créer une table si elle n'existe pas déjà
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS bike_test (
         ID SERIAL PRIMARY KEY,
@@ -30,8 +35,10 @@ cursor.execute('''
     )
 ''')
 
+#on valide les modifications apportées aux tables de la base de données
 conn.commit()
 
+#on commence une boucle infinie pour lire les données depuis la connexion série
 try:
     while True:
         line = ser.readline().decode().strip()
@@ -39,6 +46,8 @@ try:
         if line == '':
             continue
 
+#on lit une ligne de données depuis le port série et on essaie de diviser la ligne 
+# de données en valeurs en utilisant la virgule comme séparateur
         try:
             values = line.split(',')
             bike_count = 1
@@ -51,6 +60,8 @@ try:
 
         current_time = datetime.now().strftime('%H:%M:%S')
 
+#on insère les données collectées dans la table appropriée en fonction du seuil de niveau de dB défini 
+# pour chaque catégorie
         # if val > 150:
         if val > 65:
             cursor.execute('''
